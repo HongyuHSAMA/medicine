bea7344e9182a8d2fac7c58b6fa6a2863d471087
Huangyuhong <475454318@qq.com>
2017-12-20 16:57:51 +0800

    添加一个登录页面

--- src/main/java/com/design/controller/MedicineController.java
+++ src/main/java/com/design/controller/MedicineController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/")
+
 public class MedicineController {
+    @RequestMapping(value = "/index")
+        public ModelAndView test1() {
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("index");
 
-    @RequestMapping("/")
-    public ModelAndView test1() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
 
+            return modelAndView;
+        }
 
-        return modelAndView;
-    }
     @RequestMapping("/welcome")
     public ModelAndView welcome() {
         ModelAndView modelAndView = new ModelAndView();
