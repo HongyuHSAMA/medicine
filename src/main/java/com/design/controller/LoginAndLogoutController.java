5f5a72f981c9e2569709714cbd4b56d37f597a22
Huangyuhong <475454318@qq.com>
2017-12-20 17:01:26 +0800

    添加一个登录页面1

---
+++ src/main/java/com/design/controller/LoginAndLogoutController.java
@@ -0,0 +1,21 @@
+package com.design.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginAndLogoutController {
+
+    @RequestMapping("/")
+    public ModelAndView welcome( ){
+
+        ModelAndView modelAndView = new ModelAndView(  );
+
+        modelAndView.setViewName( "login" );
+
+        return modelAndView;
+
+    }
+}
